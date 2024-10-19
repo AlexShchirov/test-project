c227acb2832b3e8f8952856841eee44215d21659
Alexander Shchirov <shchirov1998@mail.ru>
2024-10-19 12:36:52 +0300

    microfronted arhitecture

--- src/features/filteredUsers/types/userType.tsx
+++
@@ -1,5 +0,0 @@
-export type UserType = {
-    name: string;
-    age: number;
-    city: string;
-  };
---
+++ users/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.scss'
+
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <div>Name: users</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Tailwind</div>
+  </div>
+)
+
+export default App
